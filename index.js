77f9f36c2d56875a9de90bab5cc696d318b32104
Favour Ifeanacho <favour.franki3@gmail.com>
2022-01-18 01:04:01 +0100

    Version before adding the api

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
+const resetBtn = document.getElementById("reset-btn")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const tabBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+if (leadsFromLocalStorage != null) {
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function() {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    renderLeads(myLeads)
 })
 
-function renderLeads() {
+function renderLeads(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
      // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
-                <a href ='${myLeads[i]}' target = '_blank'>${myLeads[i]}</a>
+                <a href ='${leads[i]}' target = '_blank'>${leads[i]}</a>
             </li>
         `
     }
     ulEl.innerHTML = listItems  
 }
+
+
+resetBtn.addEventListener("click", function(){
+    localStorage.removeItem("myLeads")
+    location.reload()
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    renderLeads(myLeads)
+})
